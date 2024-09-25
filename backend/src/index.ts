28bae4dfbc6dd5d7e9e43f653b32bb22c748f9dd
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-09-25 21:52:21 +0900

    エントリーポイントの修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 // エントリーポイント
 
 import app from "./app";
-
-const port = process.env.PORT ? parseInt(process.env.PORT) : 8787;
-
-console.log(`Server is running on port ${port}`);
+import { Env } from "hono";
 
 export default {
-  port,
-  fetch: app.fetch,
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    return app.fetch(request, env, ctx);
+  },
 };
