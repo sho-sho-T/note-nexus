1a20951818da2d953680e3ef9fbc8b4e3a456a65
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-10-01 22:07:22 +0900

    ちょい修正

--- backend/src/utils/passwordUtils.ts
+++ backend/src/utils/passwordUtils.ts
@@ -1,13 +1,15 @@
 import * as bcrypt from "bcryptjs";
 
-export async function hashPassword(password: string): Promise<string> {
+// パスワードのハッシュ化
+export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10; // ソルトのラウンド数を指定
   return bcrypt.hash(password, saltRounds);
-}
+};
 
-export async function comparePassword(
+// パスワードの検証
+export const comparePassword = async (
   password: string,
   hash: string
-): Promise<boolean> {
+): Promise<boolean> => {
   return bcrypt.compare(password, hash);
-}
+};
