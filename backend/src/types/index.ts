be29cd24c1d96eaffac3ed0532eb3e23e3051af8
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-10-27 15:30:06 +0900

    レスポンス用のuser型を定義

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,17 +1,22 @@
 export interface Env {
-  DB: D1Database;
-  JWT_SECRET: string;
-  NODE_ENV: string;
+	DB: D1Database;
+	JWT_SECRET: string;
+	NODE_ENV: string;
 }
 
 export interface User {
-  id: number;
-  username: string;
-  password_hash: string;
-  created_at: string;
+	id: number;
+	username: string;
+	password_hash: string;
+	created_at: string;
+}
+
+export interface SafeUser {
+	id: number;
+	username: string;
 }
 
 export interface JWTPayload {
-  userId: number;
-  exp: number; // 有効期限
+	userId: number;
+	exp: number; // 有効期限
 }
